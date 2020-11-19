1433e6378100e4a3a9c56c8a462a9f348f4823fd
Luiz Fernando da Silva <luiz.fernando.tech@gmail.com>
2020-11-19 20:34:14 +0000

    Created util class for alerts

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,7 +12,7 @@ public class Program extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/gui/View.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 400, 300));
         primaryStage.show();
     }
 
