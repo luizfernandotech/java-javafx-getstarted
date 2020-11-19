27049475fe19e6875e1a481ca7639628443a17fb
Luiz Fernando da Silva <luiz.fernando.tech@gmail.com>
2020-11-19 20:45:18 +0000

    Created a Sum GUI application

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -4,14 +4,34 @@ import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ViewController {
 
     @FXML
-    private Button btTest;
+    private TextField txtNumber1;
 
     @FXML
-    public void onBtTestAction() {
-        Alerts.showAlert("Alert Title", "Alert Header", "My Message", Alert.AlertType.INFORMATION);
+    private TextField txtNumber2;
+
+    @FXML
+    private Label labelResult;
+
+    @FXML
+    private Button btSum;
+
+    @FXML
+    public void onBtSumAction() {
+
+        try {
+            double number1 = Double.parseDouble(txtNumber1.getText());
+            double number2 = Double.parseDouble(txtNumber2.getText());
+            double sum = number1 + number2;
+            labelResult.setText(String.format("%.2f", sum));
+        } catch (NumberFormatException e) {
+            Alerts.showAlert("Error", "Parse Error", e.getMessage(), Alert.AlertType.ERROR);
+        }
+
     }
 }
